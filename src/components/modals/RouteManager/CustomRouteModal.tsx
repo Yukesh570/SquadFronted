cce80c767afb6c90ecb9759057135efc2f8884aa
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2026-05-21 13:55:51 +0545

    feat: Remove unused updateCustomRouteApi import from CustomRouteModal

--- src/components/modals/RouteManager/CustomRouteModal.tsx
+++ src/components/modals/RouteManager/CustomRouteModal.tsx
@@ -10,7 +10,6 @@ import {
 } from "../../ui/MultiSelectDropdown";
 import {
   createCustomRouteApi,
-  updateCustomRouteApi,
   updateRouteGroupApi,
   createRouteGroupApi,
   type CustomRouteData,
